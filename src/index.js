dbdf7eafcfc08b4d1d15507254c145d361918156
Muhammad Hani <mohammadhani110@gmail.com>
2023-11-08 02:17:08 +0500

    utils - Handlers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
-connectDB();
+
+connectDB()
+  .then(
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`App is listening on Port: ${process.env.PORT}`);
+    })
+  )
+  .catch((err) => {
+    console.log("MongoDB connection failed!!!", err);
+  });
